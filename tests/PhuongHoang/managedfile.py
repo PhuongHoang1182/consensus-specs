bf2626dfbe288cdc439644196a7bb10814ebe2b9
Phuon <Phuon@DESKTOP-9FRA6D3>
2021-09-07 18:12:28 +1000

    eth 2.0 testing working

---
+++ tests/PhuongHoang/managedfile.py
@@ -0,0 +1,23 @@
+#https://www.youtube.com/watch?v=Uh2ebFW8OYM
+
+with open ('tests/PhuongHoang/Ethereum.txt', 'r') as rf:
+    with open('tests/PhuongHoang/Ethereum2.txt', 'w') as wf:
+        for line in rf:
+            wf.write(line)
+    
+
+
+
+
+
+
+#with open ('tests/PhuongHoang/test.txt', 'r') as f:
+    #f_contents = f.readline()
+    #print(f_contents)
+
+    #f_contents = f.readline()
+    #print(f_contents)
+
+    #for line in f:
+    #    print(line)
+   
